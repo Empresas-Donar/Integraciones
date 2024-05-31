3ad7176235b82aabc0e226b5bcf84ca439b8fe72
Heimdallgg <alvaroshv@gmail.com>
2024-05-31 16:46:41 -0400

    Perfect wiseconn.py

--- app/services/wiseconn.py
+++ app/services/wiseconn.py
@@ -21,21 +21,17 @@ api_key = os.getenv('API_KEY')
 def fetch_data(endpoint_key):
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
-    
     config = endpoints_config[endpoint_key]
     headers = {"api_key": api_key}  
-
     params = config.get("params", {})
     if endpoint_key == "irrigations":
         params["initTime"] = yesterday.strftime("%Y-%m-%d")
         params["endTime"] = today.strftime("%Y-%m-%d")
-
     response = requests.get(config["url"], headers=headers, params=params)
     try:
         response.raise_for_status()  
         return response.json()  
-    except requests.HTTPError as e:
-        
+    except requests.HTTPError as e:   
         print(f"Error fetching data from {config['url']}: {e}")
         return None  
 
