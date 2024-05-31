7e4a85a345b1f823334451b24ed6a28190c62200
Heimdallgg <alvaroshv@gmail.com>
2024-05-31 17:06:30 -0400

    cleaning config.py

--- config.py
+++ config.py
@@ -3,17 +3,7 @@
 import os
 
 class Config:
-    
     SECRET_KEY = os.getenv('SECRET_KEY')
-
-    
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
-    
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
-
-    
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
-
-
