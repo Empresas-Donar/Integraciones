3b11c87c43f2e07f3943dd05b036c754a07a27bd
Heimdallgg <alvaroshv@gmail.com>
2024-06-06 15:55:41 -0400

    Cambio en task_scheduler para admitir ubibot

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,48 +1,47 @@
-# task schedule config
+# Schedule application executions and save the data of these executions
 
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
-from run import main as run_main
 from datetime import datetime
 import time
 from app import create_app, db
 from app.models import ExecutionLog
+from app.services.wiseconn import run_fetch_process
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[logging.StreamHandler()])
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
-app = create_app()
 
+app = create_app()
 def scheduled_job():
     with app.app_context():
-        status = 'Success'
-        try:
-            print(f"Ejecutando tarea programada a las {datetime.now()}...", flush=True)
-            run_main()
-        except Exception as e:
-            status = f'Failed: {e}'
-            print(f"Error durante la ejecución: {e}", flush=True)
-        finally:
-            # Registrar la ejecución
-            log = ExecutionLog(status=status, date=datetime.utcnow())
-            db.session.add(log)
-            db.session.commit()
+        results, status_wiseconn, status_ubibot = run_fetch_process()
+        log = ExecutionLog(
+            status_wiseconn=status_wiseconn,
+            status_ubibot=status_ubibot,
+            date=datetime.utcnow()
+        )
+        db.session.add(log)
+        db.session.commit()
+        logging.info(f"Registro añadido con estados - Wiseconn: {status_wiseconn}, Ubibot: {status_ubibot}")
+
 scheduler = BackgroundScheduler()
+def schedule_tasks():
+    scheduler.add_job(scheduled_job, 'cron', hour='07', minute='00')
+    scheduler.add_job(scheduled_job, 'cron', hour='12', minute='00')
+    scheduler.add_job(scheduled_job, 'cron', hour='18', minute='00')
+    scheduler.add_job(scheduled_job, 'cron', hour='22', minute='00')
 
-print("Programando tareas...", flush=True)
-scheduler.add_job(scheduled_job, 'cron', hour='07', minute='0')
-scheduler.add_job(scheduled_job, 'cron', hour='12', minute='0')
-scheduler.add_job(scheduled_job, 'cron', hour='18', minute='0')
-scheduler.add_job(scheduled_job, 'cron', hour='22', minute='0')
-print("Tareas programadas. Scheduler iniciará.", flush=True)
-
-scheduler.start()
-print("Scheduler iniciado.", flush=True)
-
-try:
-    while True:
-        time.sleep(20)  
-except (KeyboardInterrupt, SystemExit):
-    scheduler.shutdown()  
-    print("Scheduler detenido.", flush=True)
+if __name__ == "__main__":
+    print("Programando tareas...", flush=True)
+    schedule_tasks()
+    print("Tareas programadas. Scheduler iniciará.", flush=True)
+    scheduler.start()
+    print("Scheduler iniciado.", flush=True)
+    try:
+        while True:
+            time.sleep(20)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        print("Scheduler detenido.", flush=True)
