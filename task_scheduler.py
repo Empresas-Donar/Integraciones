5bdf3fd839d00899259471ab3641128a4ca646bc
Heimdallgg <alvaroshv@gmail.com>
2024-08-09 14:34:49 -0400

    Añado el proprio cronograma para sendgrid y asi mantener las independencias en las ejecuciones

--- task_scheduler.py
+++ task_scheduler.py
@@ -16,9 +16,17 @@ def scheduled_job():
     if result.stderr:
         logging.error(f"Errores de run.py: {result.stderr}")
 
+def scheduled_sendgrid_job():
+    logging.info("Ejecutando test_sendgrid.py...")
+    result = subprocess.run(["python3", "test_sendgrid.py"], capture_output=True, text=True)
+    logging.info(f"Salida de test_sendgrid.py: {result.stdout}")
+    if result.stderr:
+        logging.error(f"Errores de test_sendgrid.py: {result.stderr}")
+
 scheduler = BackgroundScheduler()
 
 def schedule_tasks():
+    # run.py
     scheduler.add_job(scheduled_job, 'cron', hour='01', minute='00')
     scheduler.add_job(scheduled_job, 'cron', hour='02', minute='00')
     scheduler.add_job(scheduled_job, 'cron', hour='03', minute='00')
@@ -43,6 +51,32 @@ def schedule_tasks():
     scheduler.add_job(scheduled_job, 'cron', hour='22', minute='00')
     scheduler.add_job(scheduled_job, 'cron', hour='23', minute='00')
     scheduler.add_job(scheduled_job, 'cron', hour='00', minute='00')
+
+    # sendgrid
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='00', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='01', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='02', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='03', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='04', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='05', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='06', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='07', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='08', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='09', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='10', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='11', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='12', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='13', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='14', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='15', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='16', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='17', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='18', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='19', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='20', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='21', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='22', minute='08')
+    scheduler.add_job(scheduled_sendgrid_job, 'cron', hour='23', minute='08')
 
 if __name__ == "__main__":
     print("Programando tareas...", flush=True)
