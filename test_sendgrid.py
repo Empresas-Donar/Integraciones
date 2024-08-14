28a745a402d71495a3391bd1be16a8d6e8678e75
Heimdallgg <alvaroshv@gmail.com>
2024-08-13 22:08:52 -0400

    Modificacion para que incorporé las 2 clases ya creadas

--- test_sendgrid.py
+++ test_sendgrid.py
@@ -1,24 +1,23 @@
 import os
 from sqlalchemy import create_engine
-from app import db 
-from app.mail_class import MailManager
 from sqlalchemy.orm import sessionmaker
+from app import db 
+from app.mail_class import MailManager, UbibotAlertManager 
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-mail_manager = MailManager(session)
-canales_info = mail_manager.channels_down()
-sensores_info = mail_manager.sensors_down()
+alert_manager = UbibotAlertManager(session)
+mail_manager = MailManager()
+channels_info = alert_manager.channels_down()
+sensors_info = alert_manager.sensors_down()
+channels_message = "Canales apagados:\n" + ", ".join(f"Canal: {channel_id}, Nombre: {name}" for channel_id, name in channels_info)
+email_content = f"Reporte de Canales y Sensores Fallidos\n{channels_message}\n{sensors_info}"
 
-canales_message = "Canales apagados con net=0:\n" + ", ".join(f"Canal: {channel_id}, Nombre: {name}" for channel_id, name in canales_info)
-email_content = f"Reporte de Canales y Sensores Fallidos\n{canales_message}\n{sensores_info}"
-
-if canales_info or sensores_info.strip():
-    mail_manager.enviar_email("Reporte de Canales y Sensores Fallidos", email_content)
+if channels_info or sensors_info.strip():
+    mail_manager.send_mail("Reporte de Canales y Sensores Fallidos", email_content)
 else:
     print("No hay canales ni sensores fallidos en las últimas horas. No se enviará ningún correo.")
-
 session.close()
