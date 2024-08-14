ddabebf07261452e18db1c21d36684181c83a02c
Heimdallgg <alvaroshv@gmail.com>
2024-08-13 22:10:12 -0400

    Se crean 2 clases para mejorar la modularidad y la independencia

--- app/mail_class.py
+++ app/mail_class.py
@@ -4,16 +4,32 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 from datetime import datetime, timedelta
 import pytz
 from sqlalchemy.orm import sessionmaker
-from .models import UbibotChannels, UbibotFields
 from sqlalchemy import create_engine, and_
+from .models import UbibotChannels, UbibotFields
 
 class MailManager:
-    def __init__(self, session):
+    def __init__(self):
         self.sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
         self.sg = sendgrid.SendGridAPIClient(api_key=self.sendgrid_api_key)
         self.from_email = Email("bot@empresasdonar.cl")
         self.to_email = To("gestion@empresasdonar.cl")
-        self.session = session 
+
+    def send_mail(self, subject, content):
+        mail = Mail(self.from_email, self.to_email, subject, Content("text/plain", content))
+        try:
+            response = self.sg.send(mail)
+            print(f"Email enviado! Código de estado: {response.status_code}")
+        except Exception as e:
+            print(f"Error al enviar el email: {str(e)}")
+
+class UbibotAlertManager:
+    def __init__(self, session):
+        self.session = session
+        self.chile_tz = pytz.timezone('America/Santiago')
+
+    def get_chile_time(self):
+        current_chile_time = datetime.now(self.chile_tz)
+        return current_chile_time.date(), current_chile_time.time(), (current_chile_time - timedelta(minutes=90)).time()
 
     def channels_down(self):
         filtered_data = self.session.query(UbibotChannels.channel_id, UbibotChannels.name).filter(
@@ -22,13 +38,10 @@ class MailManager:
         return [(data.channel_id, data.name) for data in filtered_data]
 
     def sensors_down(self):
-        chile_tz = pytz.timezone('America/Santiago')
-        fecha_actual_chile = datetime.now(chile_tz).date()
-        hora_actual_chile = datetime.now(chile_tz).time()
-        hora_inicio_chile = (datetime.now(chile_tz) - timedelta(minutes=90)).time()
+        date, current_time, start_time = self.get_chile_time()
         resultados = self.session.query(UbibotFields).filter(
-            UbibotFields.date == fecha_actual_chile,
-            UbibotFields.hour.between(hora_inicio_chile, hora_actual_chile),
+            UbibotFields.date == date,
+            UbibotFields.hour.between(start_time, current_time),
             and_(
                 UbibotFields.avg == 0,
                 UbibotFields.count == 0,
@@ -36,16 +49,8 @@ class MailManager:
                 UbibotFields.max == 0
             )
         ).all()
-
-        message = "Sensores apagados en las últimas horas:\n"
+        lines = ["Sensores apagados en las últimas horas:"]
         for resultado in resultados:
-            message += f"Canal: {resultado.channel_id}, Nombre: {resultado.name}, Hora: {resultado.hour}, Fecha: {resultado.date}\n"
-        return message 
+            lines.append(f"Canal: {resultado.channel_id}, Nombre: {resultado.name}, Hora: {resultado.hour}, Fecha: {resultado.date}")
+        return "\n".join(lines)
 
-    def enviar_email(self, subject, content):
-        mail = Mail(self.from_email, self.to_email, subject, Content("text/plain", content))
-        try:
-            response = self.sg.send(mail)
-            print(f"Email enviado! Código de estado: {response.status_code}")
-        except Exception as e:
-            print(f"Error al enviar el email: {str(e)}")
