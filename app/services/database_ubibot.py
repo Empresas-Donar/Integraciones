34d96569659b14cfa24d6cb5a57d3168cf1c3810
Heimdallgg <alvaroshv@gmail.com>
2024-09-02 19:08:15 -0400

    Se mejora la escalabilidad y se incorpora la re- revisión de datos

--- app/services/database_ubibot.py
+++ app/services/database_ubibot.py
@@ -2,13 +2,14 @@
 
 from app.models import UbibotChannels, UbibotSummary, UbibotFields
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError, DataError
+from sqlalchemy import tuple_
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from app import db
 import json
 import uuid
 from app.services.utils import is_valid_integer, clean_data, convert_to_chilean_time
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 def manage_data_ubi(processed_data, data_type):
@@ -76,25 +77,43 @@ def manage_data_ubi(processed_data, data_type):
             print(f"Error al insertar en la base de datos para {base_data_type}: {e}")
 
 def manage_fields_ubi(df):
+    chile_tz = pytz.timezone('America/Santiago')
+    now = datetime.now(chile_tz)
+    three_days_ago = now - timedelta(days=3)
+    
     df = df.dropna(subset=['channel_id', 'created_at'])
     df = df[df['channel_id'] < 100000]
+    df = df[df['created_at'] >= three_days_ago]
     df = df[df['count'].fillna(0).apply(lambda x: x <= 24)]
     df['avg'] = df['avg'].fillna(0)
     df['count'] = df['count'].fillna(0)
     df['min'] = df['min'].fillna(0)
     df['max'] = df['max'].fillna(0)
     data_dicts = df.to_dict(orient='records')
+
+    keys_to_check = [(row['channel_id'], row['created_at'], row['name']) for row in data_dicts]
+    existing_records = UbibotFields.query.filter(
+        tuple_(UbibotFields.channel_id, UbibotFields.created_at, UbibotFields.name).in_(keys_to_check)
+    ).all()
+    existing_records_dict = {(record.channel_id, record.created_at, record.name): record for record in existing_records}
+
     records_to_add = []
+
     for row in data_dicts:
         created_at_utc = row['created_at']
         row['created_at'] = convert_to_chilean_time(created_at_utc)
-        
-        existing_record = UbibotFields.query.filter_by(
-            channel_id=row['channel_id'],
-            created_at=row['created_at'],
-            name=row['name']
-        ).first()
-        if not existing_record:
+
+        key = (row['channel_id'], row['created_at'], row['name'])
+        existing_record = existing_records_dict.get(key)
+
+        if existing_record:
+            if existing_record.count == 0:
+                existing_record.avg = row['avg']
+                existing_record.min = row['min']
+                existing_record.max = row['max']
+                existing_record.count = row['count']
+                db.session.add(existing_record)
+        else:
             new_record = UbibotFields(
                 summary_id=row['summary_id'],
                 channel_id=row['channel_id'],
@@ -108,6 +127,7 @@ def manage_fields_ubi(df):
                 max=row['max']
             )
             records_to_add.append(new_record)
+
     if records_to_add:
         try:
             db.session.bulk_save_objects(records_to_add)
