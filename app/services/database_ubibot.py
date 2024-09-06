d1439a8e205eb19e718f6d52a8e09706fe30bbf3
Heimdallgg <alvaroshv@gmail.com>
2024-09-06 00:30:27 -0400

    Modificacion para actualizar o insertar registros

--- app/services/database_ubibot.py
+++ app/services/database_ubibot.py
@@ -2,7 +2,6 @@
 
 from app.models import UbibotChannels, UbibotSummary, UbibotFields
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError, DataError
-from sqlalchemy import tuple_
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from app import db
@@ -10,7 +9,12 @@ import json
 import uuid
 from app.services.utils import is_valid_integer, clean_data, convert_to_chilean_time
 from datetime import datetime, timedelta
-import pytz
+import logging
+from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError, DataError, SQLAlchemyError
+
+logging.basicConfig(level=logging.INFO, filename='updates.log', 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def manage_data_ubi(processed_data, data_type):
     model_mapping = {
@@ -18,22 +22,22 @@ def manage_data_ubi(processed_data, data_type):
         'summary': UbibotSummary
     }
     base_data_type = data_type.split('_')[0]
+    
     if base_data_type not in model_mapping:
         print(f"Unknown data type: {base_data_type}")
         return
+    
     model = model_mapping[base_data_type]
 
     if base_data_type == 'channels':
-        db.session.query(model).delete()
+        db.session.query(model).delete() 
         data_dict = processed_data.to_dict(orient='records')
         new_data = []
 
         for item in data_dict:
-            item['created_at'] = convert_to_chilean_time(item['created_at'])
             instance = model(**item)
             db.session.add(instance)
             new_data.append(item)
-        
         try:
             db.session.commit()
             print(f"{len(new_data)} nuevos registros insertados en {base_data_type}.")
@@ -47,25 +51,47 @@ def manage_data_ubi(processed_data, data_type):
     elif base_data_type == 'summary':
         data_dict = processed_data.to_dict(orient='records')
         cleaned_data_dict = clean_data(data_dict) 
-        new_data = []
-        for item in cleaned_data_dict:
-            if not is_valid_integer(item['channel_id']):
-                print(f"Invalid channel_id: {item['channel_id']}")
-                continue
-            if 'id' not in item or not item['id']:
-                item['id'] = uuid.uuid4().hex
-            channel_id = item['channel_id']
-            item['created_at'] = convert_to_chilean_time(item['created_at'])
-            created_at = item['created_at']
-            with db.session.no_autoflush:
-                exists = db.session.query(model).filter_by(channel_id=channel_id, created_at=created_at).first()
-            if not exists:
-                instance = model(**item)
-                db.session.add(instance)
-                new_data.append(item)
+
+        if not cleaned_data_dict:
+            print("No hay nuevos registros para insertar.")
+            return
+
         try:
+            select_query = """
+            SELECT id, created_at, channel_id, date, hour
+            FROM ubi_channel_summary
+            WHERE created_at >= CURRENT_DATE - INTERVAL '11 days';
+            """
+            existing_records = db.session.execute(text(select_query)).fetchall()
+            existing_records_set = set((row['created_at'], row['channel_id']) for row in existing_records)
+            print(f"Filtrados {len(existing_records)} registros existentes en los últimos 11 días.")
+        except Exception as e:
+            print(f"Error al filtrar registros de la base de datos: {e}")
+            return  
+
+        filtered_data = [
+            item for item in cleaned_data_dict
+            if (item['created_at'], item['channel_id']) not in existing_records_set
+        ]
+
+        if not filtered_data:
+            print("No hay nuevos registros para insertar después del filtrado.")
+            return
+
+        insert_statement = """
+        INSERT INTO ubi_channel_summary (id, created_at, channel_id, date, hour)
+        VALUES (:id, :created_at, :channel_id, :date, :hour)
+        ON CONFLICT (created_at, channel_id)
+        DO NOTHING;
+        """
+
+        try:
+            db.session.execute(
+                text(insert_statement), 
+                filtered_data  
+            )
             db.session.commit()
-            print(f"{len(new_data)} nuevos registros insertados en {base_data_type}.")
+            print(f"{len(filtered_data)} nuevos registros insertados con éxito en {base_data_type}.")
         except IntegrityError as e:
             db.session.rollback()
             print(f"Error de integridad al insertar datos en {base_data_type}: {e}")
@@ -77,61 +103,53 @@ def manage_data_ubi(processed_data, data_type):
             print(f"Error al insertar en la base de datos para {base_data_type}: {e}")
 
 def manage_fields_ubi(df):
-    chile_tz = pytz.timezone('America/Santiago')
-    now = datetime.now(chile_tz)
-    three_days_ago = now - timedelta(days=3)
-    
-    df = df.dropna(subset=['channel_id', 'created_at'])
-    df = df[df['channel_id'] < 100000]
-    df = df[df['created_at'] >= three_days_ago]
-    df = df[df['count'].fillna(0).apply(lambda x: x <= 24)]
-    df['avg'] = df['avg'].fillna(0)
-    df['count'] = df['count'].fillna(0)
-    df['min'] = df['min'].fillna(0)
-    df['max'] = df['max'].fillna(0)
+
+    try:
+
+        select_query = """
+        SELECT created_at, channel_id, name, avg, count, min, max, date, hour, summary_id
+        FROM ubi_channels_fields
+        WHERE created_at >= CURRENT_DATE - INTERVAL '11 days';
+        """
+        result = db.session.execute(text(select_query))
+        existing_records = result.fetchall() 
+        print(f"Filtrados {len(existing_records)} registros existentes de los últimos 11 días.")
+    except Exception as e:
+        print(f"Error al filtrar registros de la base de datos: {e}")
+        return  
+
+    df = df.dropna(subset=['channel_id', 'created_at']) 
+    df = df[df['channel_id'] < 100000]  
+    df = df[df['count'].fillna(0).apply(lambda x: x <= 24)]  
+    df['avg'] = df['avg'].fillna(0)  
+    df['count'] = df['count'].fillna(0) 
+    df['min'] = df['min'].fillna(0)  
+    df['max'] = df['max'].fillna(0) 
+
     data_dicts = df.to_dict(orient='records')
 
-    keys_to_check = [(row['channel_id'], row['created_at'], row['name']) for row in data_dicts]
-    existing_records = UbibotFields.query.filter(
-        tuple_(UbibotFields.channel_id, UbibotFields.created_at, UbibotFields.name).in_(keys_to_check)
-    ).all()
-    existing_records_dict = {(record.channel_id, record.created_at, record.name): record for record in existing_records}
+    insert_statement = """
+    INSERT INTO ubi_channels_fields (created_at, channel_id, name, avg, count, min, max, date, hour, summary_id)
+    VALUES (:created_at, :channel_id, :name, :avg, :count, :min, :max, :date, :hour, :summary_id)
+    ON CONFLICT (created_at, channel_id, name)
+    DO UPDATE SET 
+        avg = EXCLUDED.avg,
+        count = EXCLUDED.count,
+        min = EXCLUDED.min,
+        max = EXCLUDED.max
+    WHERE ubi_channels_fields.count < 12;
+    """
 
-    records_to_add = []
-
-    for row in data_dicts:
-        created_at_utc = row['created_at']
-        row['created_at'] = convert_to_chilean_time(created_at_utc)
-
-        key = (row['channel_id'], row['created_at'], row['name'])
-        existing_record = existing_records_dict.get(key)
-
-        if existing_record:
-            if existing_record.count == 0:
-                existing_record.avg = row['avg']
-                existing_record.min = row['min']
-                existing_record.max = row['max']
-                existing_record.count = row['count']
-                db.session.add(existing_record)
-        else:
-            new_record = UbibotFields(
-                summary_id=row['summary_id'],
-                channel_id=row['channel_id'],
-                created_at=row['created_at'],
-                date=row['date'],
-                hour=row['hour'],
-                name=row['name'],
-                avg=row['avg'],
-                count=row['count'],
-                min=row['min'],
-                max=row['max']
-            )
-            records_to_add.append(new_record)
-
-    if records_to_add:
-        try:
-            db.session.bulk_save_objects(records_to_add)
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            print(f"Error inserting records: {e}")
+    try:
+        db.session.execute(
+            text(insert_statement),  
+            data_dicts  
+        )
+        db.session.commit()  
+        print(f"{len(data_dicts)} registros insertados/actualizados con éxito.")
+    except IntegrityError as e:
+        db.session.rollback()  
+        print(f"Error al insertar/actualizar registros: {e}")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error inesperado: {e}")
