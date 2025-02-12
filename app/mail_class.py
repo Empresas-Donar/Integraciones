441df4fd1b2c70078b5187d4a712be03bc6551e8
Heimdallgg <alvaroshv@gmail.com>
2025-02-11 23:17:02 -0300

    Cambio al mail de destino

--- app/mail_class.py
+++ app/mail_class.py
@@ -8,7 +8,7 @@ class MailManager:
         self.sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
         self.sg = sendgrid.SendGridAPIClient(api_key=self.sendgrid_api_key)
         self.from_email = Email("bot@empresasdonar.cl")
-        self.to_email = To("gestion@empresasdonar.cl")
+        self.to_email = To("gestion.donar@gmail.com")
 
     def send_mail(self, subject, content):
         mail = Mail(self.from_email, self.to_email, subject, Content("text/plain", content))
